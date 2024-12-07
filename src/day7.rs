ade56f3a686b5f780bf61344ae76df00ecae047a
Stefan Todorov <stefan.b.todorov@gmail.com>
2024-12-07 17:51:47 -0600

    midway through day 7

--- src/day7.rs
+++ src/day7.rs
@@ -2,18 +2,45 @@ use crate::Solution;
 
 pub struct Day7 {}
 
+fn recur_equation_solvable(output: usize, inputs: &[usize], carry: usize) -> bool {
+    if inputs.is_empty() {
+        return output == carry;
+    }
+
+    if carry > output {
+        return false;
+    }
+
+    // try addition or multiplication of the next number
+    recur_equation_solvable(output, &inputs[1..], carry + inputs[0])
+        || recur_equation_solvable(output, &inputs[1..], carry * inputs[0])
+}
+
 impl Solution for Day7 {
     fn new() -> Self {
         Self {}
     }
 
     fn part1(&mut self, input: &str) -> String {
-        let _ = input;
-        String::new()
+        input
+            .lines()
+            .filter_map(|line| {
+                let (output, inputs) = line.split_once(": ").unwrap();
+                let inputs: Vec<_> = inputs
+                    .split_whitespace()
+                    .map(|s| s.parse::<usize>().unwrap())
+                    .collect();
+
+                let output = output.parse::<usize>().unwrap();
+
+                recur_equation_solvable(output, &inputs[1..], inputs[0]).then_some(output)
+            })
+            .sum::<usize>()
+            .to_string()
     }
 
     fn known_solution_part1(&self) -> Option<String> {
-        None
+        Some("3312271365652".to_string())
     }
 
     fn part2(&mut self, input: &str) -> String {
@@ -33,7 +60,20 @@ mod tests {
     #[test]
     fn test_part1() {
         let mut solution = Day7::new();
-        assert_eq!(solution.part1(r#""#), String::from(""));
+        assert_eq!(
+            solution.part1(
+                r#"190: 10 19
+3267: 81 40 27
+83: 17 5
+156: 15 6
+7290: 6 8 6 15
+161011: 16 10 13
+192: 17 8 14
+21037: 9 7 18 13
+292: 11 6 16 20"#
+            ),
+            String::from("3749")
+        );
     }
 
     #[test]
